8b73fe4bbb7df694469c3de9c8672639088011f1
Rushikesh Sonar <rusbharat@deloitte.com>
2025-03-07 01:58:54 +0530

    solved cors issue and changed port

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const connectToMongoose = require('./db')
 const express = require('express')
+var cors = require('cors')
 connectToMongoose();
 
 const app = express()
-const port = 5000
+const port = 3000
+app.use(cors())
 app.use(express.json())
 
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/note'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`myNotebook backend app listening on port ${port}`)
 })
